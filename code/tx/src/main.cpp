ce70235588cabde4c268e6364d9207c28bca3ea2
Christian Aceves <acevesrocha2009@gmail.com>
2026-03-12 16:43:08 +0100

    2026.03.12

--- code/tx/src/main.cpp
+++ code/tx/src/main.cpp
@@ -5,20 +5,20 @@
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
 #include <MPU6050.h>
+#include <Adafruit_LTR390.h>
 
 Adafruit_BME280 bme;
 TinyGPS gps;
 SoftwareSerial gpsSerial(4, 3);
 MPU6050 mpu;
+Adafruit_LTR390 ltr = Adafruit_LTR390();
 
 float temperature, humidity, pressure;
-
 bool gpsData = false;
 float latitude = 0.0, longitude = 0.0;
 int satellites = 0;
-
-int ax, ay, az;
-int gx, gy, gz;
+int ax, ay, az, gx, gy, gz;
+uint32_t uvRaw = 0;
 
 const float accScale = 2.0 * 9.81 / 32768.0;
 const float gyroScale = 250.0 / 32768.0;
@@ -41,7 +41,9 @@ void printData() {
     Serial.print(az * accScale); Serial.print("\t");
     Serial.print(gx * gyroScale); Serial.print("\t");
     Serial.print(gy * gyroScale); Serial.print("\t");
-    Serial.println(gz * gyroScale);
+    Serial.print(gz * gyroScale); Serial.print("\t");
+
+    Serial.println(uvRaw); 
 }
 
 void setup() {
@@ -49,25 +51,29 @@ void setup() {
     gpsSerial.begin(9600);
     Wire.begin();
 
-    if (!bme.begin(0x77)) {
-        Serial.println("No se ha encontrado el BME280");
+    if (!bme.begin()) {
+        Serial.println("Error: BME280 no detectado");
         while (1);
     }
 
     mpu.initialize();
     if (!mpu.testConnection()) {
-        Serial.println("Error al conectar MPU6050");
+        Serial.println("Error: MPU6050 no detectado");
         while (1);
     }
 
-    Serial.println("Temp\tHum\tPres\tGPS\tLat\tLon\tSat\tax\tay\taz\tgx\tgy\tgz");
+    if (!ltr.begin()) {
+        Serial.println("Error: LTR390 no detectado");
+        while (1);
+    }
+    ltr.setMode(LTR390_MODE_UVS);
+    ltr.setGain(LTR390_GAIN_3);
+    ltr.setResolution(LTR390_RESOLUTION_16BIT);
+
+    Serial.println("Temp\tHum\tPres\tGPS\tLat\tLon\tSat\tax\tay\taz\tgx\tgy\tgz\tUV");
 }
 
 void loop() {
-    temperature = bme.readTemperature();
-    humidity = bme.readHumidity();
-    pressure = bme.readPressure() / 100.0F;
-
     while (gpsSerial.available()) {
         char c = gpsSerial.read();
         if (gps.encode(c)) {
@@ -75,22 +81,32 @@ void loop() {
         }
     }
 
-    if (gpsData) {
-        unsigned long age;
-        gps.f_get_position(&latitude, &longitude, &age);
-
-        if (latitude == TinyGPS::GPS_INVALID_F_ANGLE) latitude = 0.0;
-        if (longitude == TinyGPS::GPS_INVALID_F_ANGLE) longitude = 0.0;
-
-        satellites = gps.satellites();
-        if (satellites == TinyGPS::GPS_INVALID_SATELLITES) satellites = 0;
-    }
-
-    mpu.getAcceleration(&ax, &ay, &az);
-    mpu.getRotation(&gx, &gy, &gz);
-
     if (millis() - lastPrint >= printInterval) {
-        printData();
         lastPrint = millis();
+
+        temperature = bme.readTemperature();
+        humidity = bme.readHumidity();
+        pressure = bme.readPressure() / 100.0F;
+
+        mpu.getAcceleration(&ax, &ay, &az);
+        mpu.getRotation(&gx, &gy, &gz);
+
+        if (ltr.newDataAvailable()) {
+            uvRaw = ltr.readUVS();
+        }
+
+        if (gpsData) {
+            unsigned long age;
+            gps.f_get_position(&latitude, &longitude, &age);
+            satellites = gps.satellites();
+
+            if (latitude == TinyGPS::GPS_INVALID_F_ANGLE) latitude = 0.0;
+            if (longitude == TinyGPS::GPS_INVALID_F_ANGLE) longitude = 0.0;
+            if (satellites == TinyGPS::GPS_INVALID_SATELLITES) satellites = 0;
+            
+            if (age > 2000) gpsData = false;
+        }
+
+        printData();
     }
 }
